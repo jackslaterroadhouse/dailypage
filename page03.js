ca030d78fa84fe480a06cf0b6fab653be9641355
Jack Slater <jackslaterroadhouse@gmail.com>
2017-11-16 14:06:43 -0800

    Title fix

---
+++ page03.js
@@ -0,0 +1,19 @@
+
+function color1() {
+	document.body.style.backgroundColor = "#565656";
+	}
+
+function color2() {
+	document.body.style.backgroundColor = "#D7CEC7";
+	}
+
+function color3() {
+	document.body.style.backgroundColor = "#FA8072";
+	}
+	
+function color4() {
+	document.body.style.backgroundColor = "#808000";
+	}
+
+
+
